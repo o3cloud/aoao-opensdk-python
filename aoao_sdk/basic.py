6b9e8df05db23902bdcf8db2f255bf142fb4b956
zhengjiwen <zjw2227680283@qq.com>
2016-12-26 11:49:13 +0800

    添加create默认参数

--- aoao_sdk/basic.py
+++ aoao_sdk/basic.py
@@ -9,10 +9,16 @@ class AoaoBasic(AoaoBase, RequestsClient):
     :param access_key: 公钥（ 由嗷嗷对接人员提供 )，用于数据传输，及身份验证
     :param secret_key: 私钥（ 由嗷嗷对接人员提供 )，用于计算签名
     """
+
     def __init__(self, access_key=None, secret_key=None):
         super(AoaoBasic, self).__init__(access_key, secret_key)
 
-    def create(self, **kwargs):
+    def create(self, org_id, org_order_id, contract_id, pay_type, city_code, org_order_pushed_at=None,
+               shipping_date=None,
+               shipping_time=None, goods=None, total_weight=None, total_volume=None, barcode=None,
+               preserve_mode=None, note=None, pickup_code=None, recv_code=None, order_amonut=None, cod_amonut=None,
+               pay_amount=None, extra_metas=None, address_id=None, consignor=None,
+               ):
         """嗷嗷下单接口
 
         商户可通过此接口向嗷嗷平台推送订单，平台接收到请求后会立即告知订单是否接收成功，然后会通过推送接口告知订单的实时状态
@@ -24,18 +30,17 @@ class AoaoBasic(AoaoBase, RequestsClient):
         :param kwargs:
         :return:
         """
+
         cmd = 'aoao.o2o.order.create'
-        need_params_create = [
-            'org_id',
-            'org_order_id',
-            'contract_id',
-            'pay_type',
-            'consignee',
-            'city_code',
-        ]
-        check_result = self.check_dictvalue(need_params_create, **kwargs)
-        if check_result is not True:
-            return check_result
-        data = self.get_aoao_object(cmd, **kwargs)
+
+        body = {
+            'org_id': org_id,
+            'org_order_id': org_order_id,
+            'contract_id': contract_id,
+            'pay_type': pay_type,
+            'city_code': city_code,
+        }
+
+        data = self.get_aoao_object(cmd, **body)
         r = self.request(data)
         return r
